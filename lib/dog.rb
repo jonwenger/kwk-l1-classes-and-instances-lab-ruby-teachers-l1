28a7fbfba39ec49db7b99a42c6c5f25da7b85056
Jon Wenger <jonwenger783@gmail.com>
2018-06-07 14:12:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # dog.rb
+class Dog
+  
+  def initialize (name)
+    @name = name 
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  puts "woof, my name is #{name}"
+end 
+
+fido = Dog.new("Fido")
+
