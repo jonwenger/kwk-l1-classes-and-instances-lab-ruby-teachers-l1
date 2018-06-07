417211418b8c1b5e5a6dd4d07dbbad3194b850e6
Jon Wenger <jonwenger783@gmail.com>
2018-06-07 14:26:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,13 @@
 # dog.rb
 class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+
+  def name
+    this_dogs_name
+  end
   
-  def name= (dog_name)
-    this_dogs_name = name 
-  end 
-  
-  def name 
-   this_dogs_name 
-  end 
-  
-  puts "woof, my name is #{this_dogs_name}"
-  
-end 
+end
 
 fido = Dog.new("Fido")
